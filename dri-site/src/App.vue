16187292591917093afdbac68ec968bc3d4eabea
Kyle <kidabrea@gmail.com>
2021-09-03 20:31:42 -0700

    fixed UI; form validation in progress

--- dri-site/src/App.vue
+++ dri-site/src/App.vue
@@ -1,17 +1,20 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <Registration/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import Registration from './components/Registration.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    // HelloWorld,
+    Registration
   }
 }
 </script>
